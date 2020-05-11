7a4cd441db4753f56526c6720682c6a3e2ce6baf
Luke Spaans <luke-spaans@hotmail.com>
2020-05-11 13:04:20 +0200

    Update DataSet to list.

--- src/Datasets/DataSet.java
+++ src/Datasets/DataSet.java
@@ -3,10 +3,11 @@ package Datasets;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DataSet {
-    public void readFile(String filePath) throws FileNotFoundException {
+    public List<Integer> readFile(String filePath) throws FileNotFoundException {
         Scanner s = new Scanner(new File(filePath));
         ArrayList<Integer> list = new ArrayList<Integer>();
         while (s.hasNext()) {
@@ -14,5 +15,6 @@ public class DataSet {
                 list.add(s.nextInt());
             }
         }
+        return list;
     }
 }
