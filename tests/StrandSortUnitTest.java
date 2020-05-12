aee2216a89c864490007512049b334fa7627e56d
Koen van der Fluit <koenvanderfluit@hotmail.com>
2020-05-12 14:54:16 +0200

    Fixed size tests for StrandSort

--- tests/StrandSortUnitTest.java
+++ tests/StrandSortUnitTest.java
@@ -3,7 +3,6 @@ import nl.hva.Strand;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
 
@@ -14,6 +13,7 @@ public class StrandSortUnitTest  {
     private LinkedList<Integer> middleList = (LinkedList<Integer>) dataset.readFile("src/datasets/middleDataSet.txt");
     private LinkedList<Integer> bigList = (LinkedList<Integer>) dataset.readFile("src/datasets/bigDataSet.txt");
     private LinkedList<Integer> biggestList = (LinkedList<Integer>) dataset.readFile("src/datasets/biggestDataSet.txt");
+    private int beginSize = 0;
 
     public StrandSortUnitTest() throws FileNotFoundException {
     }
@@ -46,28 +46,30 @@ public class StrandSortUnitTest  {
     // Size tests
     @Test
     public void SmallDataResultIsSameSizeAsStartList() {
+        beginSize = smallestList.size();
         LinkedList<Integer> resultList = Strand.strandSort(smallestList);
-        System.out.println(resultList.size());
-        System.out.println(smallestList.size());
-        assertTrue(resultList.size() == smallestList.size());
+        assertEquals(resultList.size(), beginSize);
     }
 
     @Test
     public void MediumDataResultIsSameSizeAsStartList() {
+        beginSize = middleList.size();
         LinkedList<Integer> resultList = Strand.strandSort(middleList);
-        assertEquals(resultList.size(), middleList.size());
+        assertEquals(resultList.size(), beginSize);
     }
 
     @Test
     public void BigDataResultIsSameSizeAsStartList() {
+        beginSize = bigList.size();
         LinkedList<Integer> resultList = Strand.strandSort(bigList);
-        assertEquals(resultList.size(), bigList.size());
+        assertEquals(resultList.size(), beginSize);
     }
 
     @Test
     public void BiggestDataResultIsSameSizeAsStartList() {
+        beginSize = biggestList.size();
         LinkedList<Integer> resultList = Strand.strandSort(biggestList);
-        assertEquals(resultList.size(), biggestList.size());
+        assertEquals(resultList.size(), beginSize);
     }
 
     private boolean isArraySortedAscending(LinkedList<Integer> linkedList){
