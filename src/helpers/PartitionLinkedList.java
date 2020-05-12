1a82d120cfa8a7fbdab9bbb55eb17ea96700e3a2
Luke Spaans <luke-spaans@hotmail.com>
2020-05-12 16:17:40 +0200

    Formatted

--- src/helpers/PartitionLinkedList.java
+++ src/helpers/PartitionLinkedList.java
@@ -4,21 +4,17 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class PartitionLinkedList {
-    public static <Integer> List<List<Integer>> partition(LinkedList<Integer> numberList, int maxSize)
-    {
+    public static <Integer> List<List<Integer>> partition(LinkedList<Integer> numberList, int maxSize) {
         List<List<Integer>> res = new LinkedList<>();
         List<Integer> internal = new LinkedList<>();
-        for (Integer number : numberList)
-        {
+        for (Integer number : numberList) {
             internal.add(number);
-            if (internal.size() == maxSize)
-            {
+            if (internal.size() == maxSize) {
                 res.add(internal);
                 internal = new LinkedList<>();
             }
         }
-        if (internal.isEmpty() == false)
-        {
+        if (internal.isEmpty() == false) {
             res.add(internal);
         }
         return res;
