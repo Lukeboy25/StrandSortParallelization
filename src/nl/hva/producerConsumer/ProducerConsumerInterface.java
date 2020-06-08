6e0704c2ede369dc21992eefd37c59e594e63975
Luke Spaans <luke-spaans@hotmail.com>
2020-06-08 14:26:10 +0200

    rename parameters ProducerConsumerInterface

--- src/nl/hva/producerConsumer/ProducerConsumerInterface.java
+++ src/nl/hva/producerConsumer/ProducerConsumerInterface.java
@@ -3,7 +3,7 @@ package nl.hva.producerConsumer;
 import java.util.LinkedList;
 
 public interface ProducerConsumerInterface {
-    LinkedList<Integer> produce(LinkedList<Integer> t) throws InterruptedException;
+    LinkedList<Integer> produce(LinkedList<Integer> produceList) throws InterruptedException;
 
-    LinkedList<Integer> consume(LinkedList<Integer> t) throws InterruptedException;
+    LinkedList<Integer> consume(LinkedList<Integer> consumeList) throws InterruptedException;
 }
