9ba5154beab88e16de0940a37d42c98acab0eb7a
Luke Spaans <luke-spaans@hotmail.com>
2020-06-08 14:58:51 +0200

    refactor ProducerConsumerInterface

--- src/nl/hva/producerConsumer/ProducerConsumerInterface.java
+++ src/nl/hva/producerConsumer/ProducerConsumerInterface.java
@@ -3,7 +3,7 @@ package nl.hva.producerConsumer;
 import java.util.LinkedList;
 
 public interface ProducerConsumerInterface {
-    LinkedList<Integer> produce(LinkedList<Integer> produceList) throws InterruptedException;
+   void produce(LinkedList<Integer> produceList) throws InterruptedException;
 
-    LinkedList<Integer> consume(LinkedList<Integer> consumeList) throws InterruptedException;
+    void consume() throws InterruptedException;
 }
