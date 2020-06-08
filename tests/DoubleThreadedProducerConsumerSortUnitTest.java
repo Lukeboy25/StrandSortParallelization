223e4ecbd6a5421b34cfe5045c5c52df9e64f7ff
Luke Spaans <luke-spaans@hotmail.com>
2020-06-08 15:45:45 +0200

    Add DoubleThreadedProducerConsumerSortUnitTest.

---
+++ tests/DoubleThreadedProducerConsumerSortUnitTest.java
@@ -0,0 +1,108 @@
+import datasets.TextFileReader;
+import nl.hva.producerConsumer.DoubleThreadedProducerConsumerSort;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DoubleThreadedProducerConsumerSortUnitTest {
+
+    DoubleThreadedProducerConsumerSort doubleThreadedProducerConsumer = new DoubleThreadedProducerConsumerSort();
+    private LinkedList<Integer> smallestList = TextFileReader.readFile("src/datasets/smallestDataSet.txt");
+    private LinkedList<Integer> middleList = TextFileReader.readFile("src/datasets/middleDataSet.txt");
+    private LinkedList<Integer> bigList = TextFileReader.readFile("src/datasets/bigDataSet.txt");
+    private LinkedList<Integer> biggestList = TextFileReader.readFile("src/datasets/biggestDataSet.txt");
+    private int beginSize = 0;
+
+    private LinkedList<Integer> biggestJavaSortedInputList = new LinkedList<>(biggestList);
+
+    public DoubleThreadedProducerConsumerSortUnitTest() throws FileNotFoundException {}
+
+    @Test
+    public void StrandSortResultIsSameAsDefaultCollectionSort() throws InterruptedException {
+        Collections.sort(biggestJavaSortedInputList);
+        doubleThreadedProducerConsumer.produce(biggestJavaSortedInputList);
+        doubleThreadedProducerConsumer.consume();
+
+        assertEquals(doubleThreadedProducerConsumer.resultList, biggestJavaSortedInputList);
+    }
+
+    // Ascending tests
+    @Test
+    public void SmallDataSetIsSortedAscending() throws InterruptedException {
+        doubleThreadedProducerConsumer.produce(smallestList);
+        doubleThreadedProducerConsumer.consume();
+        assertTrue(isArraySortedAscending(doubleThreadedProducerConsumer.resultList));
+    }
+
+    @Test
+    public void MediumSetIsSortedAscending() throws InterruptedException {
+        doubleThreadedProducerConsumer.produce(middleList);
+        doubleThreadedProducerConsumer.consume();
+        assertTrue(isArraySortedAscending(doubleThreadedProducerConsumer.resultList));
+    }
+
+    @Test
+    public void BigDataSetIsSortedAscending() throws InterruptedException {
+        doubleThreadedProducerConsumer.produce(bigList);
+        doubleThreadedProducerConsumer.consume();
+        assertTrue(isArraySortedAscending(doubleThreadedProducerConsumer.resultList));
+    }
+
+    @Test
+    public void BiggestDataSetIsSortedAscending() throws InterruptedException {
+        doubleThreadedProducerConsumer.produce(biggestList);
+        doubleThreadedProducerConsumer.consume();
+        assertTrue(isArraySortedAscending(doubleThreadedProducerConsumer.resultList));
+    }
+
+    // Size tests
+    @Test
+    public void SmallDataResultIsSameSizeAsStartList() throws InterruptedException {
+        beginSize = smallestList.size();
+        doubleThreadedProducerConsumer.produce(smallestList);
+        doubleThreadedProducerConsumer.consume();
+        assertEquals(beginSize, doubleThreadedProducerConsumer.resultList.size());
+    }
+
+    @Test
+    public void MediumDataResultIsSameSizeAsStartList() throws InterruptedException {
+        beginSize = middleList.size();
+        doubleThreadedProducerConsumer.produce(middleList);
+        doubleThreadedProducerConsumer.consume();
+        assertEquals(beginSize, doubleThreadedProducerConsumer.resultList.size());
+    }
+
+    @Test
+    public void BigDataResultIsSameSizeAsStartList() throws InterruptedException {
+        beginSize = bigList.size();
+        doubleThreadedProducerConsumer.produce(bigList);
+        doubleThreadedProducerConsumer.consume();
+        assertEquals(beginSize, doubleThreadedProducerConsumer.resultList.size());
+    }
+
+    @Test
+    public void BiggestDataResultIsSameSizeAsStartList() throws InterruptedException {
+        beginSize = biggestList.size();
+        doubleThreadedProducerConsumer.produce(biggestList);
+        doubleThreadedProducerConsumer.consume();
+        assertEquals(beginSize, doubleThreadedProducerConsumer.resultList.size());
+    }
+
+    private boolean isArraySortedAscending(LinkedList<Integer> linkedList) {
+        Integer[] array = new Integer[linkedList.size()];
+        array = linkedList.toArray(array);
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return false; //return when the current element is bigger than te next element
+            }
+        }
+
+        return true;
+    }
+}
