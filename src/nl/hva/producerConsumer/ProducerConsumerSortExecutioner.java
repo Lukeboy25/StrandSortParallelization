fb9cc476a43ebfeb00e581f6236b41fd2331a0e8
Luke Spaans <luke-spaans@hotmail.com>
2020-06-09 14:45:41 +0200

    refactor for BetterCode.

--- src/nl/hva/producerConsumer/ProducerConsumerSortExecutioner.java
+++ src/nl/hva/producerConsumer/ProducerConsumerSortExecutioner.java
@@ -9,23 +9,23 @@ import java.util.LinkedList;
 public class ProducerConsumerSortExecutioner {
 
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-        ProducerConsumerHelper producerConsumerHelper = new ProducerConsumerHelper();
         int amountOfThreads = 8;
 
-        long elapsedSmallestTime = calculateTime(producerConsumerHelper, amountOfThreads, "src/datasets/smallestDataSet.txt");
+        long elapsedSmallestTime = calculateTime(amountOfThreads, "src/datasets/smallestDataSet.txt");
         System.out.println("Sorting time for 1000 in milliseconds: " + (System.currentTimeMillis() - elapsedSmallestTime));
 
-        long elapsedMiddleTime = calculateTime(producerConsumerHelper, amountOfThreads, "src/datasets/middleDataSet.txt");
+        long elapsedMiddleTime = calculateTime(amountOfThreads, "src/datasets/middleDataSet.txt");
         System.out.println("Sorting time for 10.000 in milliseconds: " + (System.currentTimeMillis() - elapsedMiddleTime));
 
-        long elapsedBigTime = calculateTime(producerConsumerHelper, amountOfThreads, "src/datasets/bigDataSet.txt");
+        long elapsedBigTime = calculateTime(amountOfThreads, "src/datasets/bigDataSet.txt");
         System.out.println("Sorting time for 100.000 in milliseconds: " + (System.currentTimeMillis() - elapsedBigTime));
 
-        long elapsedBiggestTime = calculateTime(producerConsumerHelper, amountOfThreads,"src/datasets/biggestDataSet.txt");
+        long elapsedBiggestTime = calculateTime(amountOfThreads,"src/datasets/biggestDataSet.txt");
         System.out.println("Sorting time for 250.000 in milliseconds: " + (System.currentTimeMillis() - elapsedBiggestTime));
     }
 
-    private static long calculateTime(ProducerConsumerHelper producerConsumerHelper, int amountOfThreads, String filePath) throws FileNotFoundException, InterruptedException {
+    private static long calculateTime(int amountOfThreads, String filePath) throws FileNotFoundException, InterruptedException {
+        ProducerConsumerHelper producerConsumerHelper = new ProducerConsumerHelper();
         LinkedList<Integer> unsortedList = TextFileReader.readFile(filePath);
 
         long elapsedTime = System.currentTimeMillis();
